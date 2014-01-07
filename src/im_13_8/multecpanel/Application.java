616335a6ad651df30888005ea021ccc8ccee6c80
sam-coenen <sam.coenen@student.ehb.be>
2014-01-07 14:11:24 +0100

    Temporary code for database access

--- src/im_13_8/multecpanel/Application.java
+++ src/im_13_8/multecpanel/Application.java
@@ -46,7 +46,7 @@ public class Application extends MTApplication {
 		
 		
 		this.frameRate(60);
-		this.addScene(new Menu(this, "studie"));
+		this.addScene(new ListView(this, "studie"));
 	}
 
 	public static void main(String[] args) {
--- src/im_13_8/multecpanel/controller/DetailController.java
+++ src/im_13_8/multecpanel/controller/DetailController.java
@@ -4,7 +4,39 @@ import im_13_8.multecpanel.entiteiten.DetailInfo;
 
 import java.util.ArrayList;
 
+/*
+import java.sql.*;
+import com.mysql.jdbc.PreparedStatement;
+
 public class DetailController {
+	public DetailInfo getDetailInfo() throws Exception {
+		// Access drivers from JAR file
+		Class.forName("com.mysql.jdbc.Driver");
+	
+		// Creating variable for connection "con"
+		Connection con = DriverManager.getConnection(
+				"jdbc:mysql://iwt5.ehb.be/multiscreen", "multiscreen", "Sm358");
+	
+		// Creating query
+		java.sql.PreparedStatement statement = con.prepareStatement("select * from vak");
+	
+		// Create variable to execute query
+		ResultSet result = statement.executeQuery();
+	
+		DetailInfo detail;
+		while (result.next()) {
+			detail = new DetailInfo(result.getString(2), result.getString(3), result.getString(4));
+		}
+		
+		return detail;
+	}
+}
+ */
+
+
+
+public class DetailController {
+
 	
 	public DetailInfo getDetailInfo(){
 		
