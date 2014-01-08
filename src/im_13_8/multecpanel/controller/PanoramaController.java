f4ae9aea9a310cc913a8f3403fd8461a737ad010
johansergeyssels <johan@johans-MacBook-Pro.local>
2014-01-08 16:32:41 +0100

    panoramaview
    panoramaview

--- src/im_13_8/multecpanel/controller/PanoramaController.java
+++ src/im_13_8/multecpanel/controller/PanoramaController.java
@@ -7,15 +7,15 @@ import java.util.ArrayList;
 public class PanoramaController {
 	public ArrayList<ListItem> getListitems() {
 		ArrayList<ListItem> items = new ArrayList<ListItem>();
-		items.add(new ListItem("images/locaties_thumbnails/classRoom.jpg", "B103", "B", "afbeelding", "testen"));
-		items.add(new ListItem("images/locaties_thumbnails/edited_pano_fablab.jpg", "fablab", "B", "afbeelding", "testen"));
-		items.add(new ListItem("images/locaties_thumbnails/inkom.jpg", "inkom", "A", "afbeelding", "testen"));
-		items.add(new ListItem("images/locaties_thumbnails/Panorama-parking.jpg", "parking", "buiten", "afbeelding", "testen"));
-		items.add(new ListItem("images/locaties_thumbnails/Panorama-audi.jpg", "auditorium", "A", "afbeelding", "testen2"));
-		items.add(new ListItem("images/locaties_thumbnails/Panorama-resto.jpg", "resto", "D", "afbeelding", "testen2"));
-		items.add(new ListItem("images/locaties_thumbnails/Panorama-verdieping.jpg", "labyrint", "A", "afbeelding", "testen3"));
-		items.add(new ListItem("images/locaties_thumbnails/Panorama-verdieping+.jpg", "gang", "A", "afbeelding", "testen3"));
-		items.add(new ListItem("images/locaties_thumbnails/Panorama4_grasveld.jpg", "plein", "buiten", "afbeelding", "testen3"));
+		items.add(new ListItem("images/locaties_thumbnails/classRoom.jpg", "B103", "B", "images/pano/classRoom.jpg", "testen"));
+		items.add(new ListItem("images/locaties_thumbnails/edited_pano_fablab.jpg", "fablab", "B", "images/pano/inkom.jpg", "testen"));
+		items.add(new ListItem("images/locaties_thumbnails/inkom.jpg", "inkom", "A", "images/pano/pano_fablab.jpg", "testen"));
+		items.add(new ListItem("images/locaties_thumbnails/Panorama-parking.jpg", "parking", "buiten", "images/pano/Panorama-audi.jpg", "testen"));
+		items.add(new ListItem("images/locaties_thumbnails/Panorama-audi.jpg", "auditorium", "A", "images/pano/Panorama-bib.jpg", "testen2"));
+		items.add(new ListItem("images/locaties_thumbnails/Panorama-resto.jpg", "resto", "D", "images/pano/Panorama-parking.jpg", "testen2"));
+		items.add(new ListItem("images/locaties_thumbnails/Panorama-verdieping.jpg", "labyrint", "A", "images/pano/Panorama-resto.jpg", "testen3"));
+		items.add(new ListItem("images/locaties_thumbnails/Panorama-verdieping+.jpg", "gang", "A", "images/pano/Panorama-verdieping.jpg", "testen3"));
+		items.add(new ListItem("images/locaties_thumbnails/Panorama4_grasveld.jpg", "plein", "buiten", "images/pano/Panorama4_grasveld.jpg", "testen3"));
 		return items;
 	}
 }
--- src/im_13_8/multecpanel/view/panorama/PanoramaView.java
+++ src/im_13_8/multecpanel/view/panorama/PanoramaView.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 import org.mt4j.components.TransformSpace;
 import org.mt4j.components.visibleComponents.shapes.MTRectangle;
 import org.mt4j.components.visibleComponents.shapes.MTRectangle.PositionAnchor;
+import org.mt4j.input.inputProcessors.IGestureEventListener;
+import org.mt4j.input.inputProcessors.MTGestureEvent;
+import org.mt4j.input.inputProcessors.componentProcessors.dragProcessor.DragEvent;
+import org.mt4j.input.inputProcessors.componentProcessors.dragProcessor.DragProcessor;
 import org.mt4j.util.math.Vector3D;
 
 import processing.core.PImage;
@@ -12,6 +16,7 @@ import im_13_8.multecpanel.Application;
 import im_13_8.multecpanel.controller.PanoramaController;
 import im_13_8.multecpanel.entiteiten.ListItem;
 import im_13_8.multecpanel.entiteiten.ParentEntiteit;
+import im_13_8.multecpanel.view.list.IListItemSliderObserver;
 import im_13_8.multecpanel.view.list.ListItemSliderView;
 import im_13_8.multecpanel.view.util.BackButton;
 import im_13_8.multecpanel.view.util.CustomScene;
@@ -29,28 +34,47 @@ public class PanoramaView extends CustomScene {
 		MTRectangle transBox = new MTRectangle(0, app.height / 15 * 11, app.width, app.height / 15 * 4, app);
 		transBox.setFillColor(app.getTransparantBlack());
 		transBox.setNoStroke(true);
+		transBox.removeAllGestureEventListeners();
 		this.getCanvas().addChild(transBox);
 		
+		final Application application = app;
 		ListItemSliderView listitemsliderview = new ListItemSliderView(0, app.height / 15 * 11, app.width, 250, items, app, 250, -270);
+		listitemsliderview.registerListItemSliderObserver(new IListItemSliderObserver() {
+			
+			@Override
+			public void listItemSelected(ListItemSliderView view, ListItem item) {
+				loadImage(item.getSoort(), application);
+			}
+		});
 		this.getCanvas().addChild(listitemsliderview);
 		
 		BackButton backButton = new BackButton(app, this);
 		this.getCanvas().addChild(backButton);
 		
-		loadImage("images/panoramas/classRoom.png", app);
 	}
 	
 	private void loadImage(String path, Application app) {
 		PImage image = app.loadImage(path);
 		
-		MTRectangle panorama = new MTRectangle(0, 0,  image.width, image.height, app);
+		
+		panorama = new MTRectangle(0, 0,  image.width, image.height, app);
 		panorama.removeAllGestureEventListeners();
 		panorama.setAnchor(PositionAnchor.CENTER);
-		panorama.setPositionRelativeToParent(new Vector3D(0, 0, -1));
+		panorama.setNoStroke(true);
+		panorama.setPositionRelativeToParent(new Vector3D(app.width / 2, app.height / 2, -1));
 		panorama.setTexture(image);
 		panorama.setWidthXYRelativeToParent(image.width);
 		panorama.setHeightXYRelativeToParent(image.height);
-		System.out.println(panorama.getWidthXY(TransformSpace.RELATIVE_TO_PARENT));
+		panorama.addGestureListener(DragProcessor.class, new IGestureEventListener() {
+			
+			@Override
+			public boolean processGestureEvent(MTGestureEvent ge) {
+				DragEvent event = (DragEvent)ge;
+				event.getTranslationVect().setY(0);
+				panorama.translate(event.getTranslationVect());
+				return false;
+			}
+		});
 		this.getCanvas().addChild(panorama);
 	}
 
